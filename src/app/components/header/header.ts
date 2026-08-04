99892194281367d5c163e103b38175da65e077bf
edmond <noumbissiekengniedmond@gmail.com>
2026-08-04 07:10:58 +0100

    Mise a jour

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class HeaderComponent {
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 
 }
